e1734e66cba4bc4fa43ec9405d6131427dc686b7
Jonas Wolfram <jonas-wolfram@web.de>
2022-05-16 22:54:20 +0700

    Initial commit

---
+++ scripts/run.js
@@ -0,0 +1,22 @@
+import hardhat from 'hardhat';
+
+const main = async () => {
+  const nftContractFactory = await hardhat.ethers.getContractFactory(
+    'DeemoNFT'
+  );
+  const nftContract = await nftContractFactory.deploy();
+  await nftContract.deployed();
+  console.log('Contract deployed to:', nftContract.address);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
